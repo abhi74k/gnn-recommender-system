5c24603e8b9c0fc5927ee4201c62d28cb7afde75
abhi74k <abhi74k@gmail.com>
2023-12-14 17:30:39 +0000

    Commit links to dictionary files

---
+++ datadictionary.py
@@ -0,0 +1,11 @@
+dictionary = {
+    "image_embeddings", "https://www.dropbox.com/scl/fi/j9h9j3snrsv2ejc98ho6y/image_embeddings_k_5.npy?rlkey=lxsoy0c8hksz73tr1ztr8mqr7&dl=0",
+    "text_embeddings", "https://www.dropbox.com/scl/fi/ftjz6nnamdb2c8xzibmst/text_embeddings_k_5.npy?rlkey=dejp5lpb530ibcmktg7xinmas&dl=0",
+    "image_embeddings_brand", "https://www.dropbox.com/scl/fi/nw1pesildeziph5v236ym/image_embeddings.npy_k_5_brand.npy?rlkey=wbbn8pfmpbr9m7pkh31ubzqr4&dl=0",
+    "text_embeddings_brand", "https://www.dropbox.com/scl/fi/a2xwys9ylvpu7y2owa14r/text_embeddings_k_5.npy_brand.npy?rlkey=o1dose2xvxsc8q1t7ystdi42d&dl=0",
+    "brand_embeddings", "https://www.dropbox.com/scl/fi/98v7oaiy083twtuywzzml/brand_embeddings_k_5_brand.npy?rlkey=m2w00qga8pweexxcrpto2aai9&dl=0",
+    "kcore5_csv", "https://www.dropbox.com/scl/fi/0e1ycobbjk35pk7cvr0bs/kcore5new.csv?rlkey=fksob5odaactxb1pm5fkauij9&dl=0",
+    "edge_index", "https://www.dropbox.com/scl/fi/t6ed0unoxoxxv7zm6sgwc/k5_edge_index.npy?rlkey=cag18rojre6aeykjd3d4en2jb&dl=0",
+    "kcore5_brand_csv", "https://www.dropbox.com/scl/fi/gxx7ijkd7dpfei1bljizi/kcore5new_brand.csv?rlkey=jdmzcp18i9sbn30mdjy6dlqii&dl=0",
+    "edge_index_brand", "https://www.dropbox.com/scl/fi/czohscd2oettq9loby7ic/k5_edge_index_brand.npy?rlkey=l0igrsyh1ia6wcrswmn7fqvz0&dl=0",
+}    
